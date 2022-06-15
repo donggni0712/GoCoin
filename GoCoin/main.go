dce29b1915741bd2aac53489219535db20d6d35f
donggni0712 <donggni0712@naver.com>
2022-06-15 19:31:40 +0900

    :ice_cube:Make addBlock Function

--- GoCoin/main.go
+++ GoCoin/main.go
@@ -20,11 +20,40 @@ type block struct {
 	*/
 }
 
+type blockchain struct {
+	blocks []block
+}
+
+//가장 최신의 hash가져오기
+func (b *blockchain) getLastHash() string {
+	if len(b.blocks) > 0 {
+		return b.blocks[len(b.blocks)-1].hash
+	}
+	return ""
+}
+
+//블록 추가
+func (b *blockchain) addBlock(data string) {
+	newBlock := block{data, "", b.getLastHash()}
+	hash := sha256.Sum256([]byte(newBlock.data + newBlock.prevHash))
+	newBlock.hash = fmt.Sprintf("%x", hash)
+	b.blocks = append(b.blocks, newBlock)
+}
+
+//블록체인 출력
+func (b *blockchain) listBlocks() {
+	for _, block := range b.blocks {
+		fmt.Printf("Data: %s\n", block.data)
+		fmt.Printf("Hash: %s\n", block.hash)
+		fmt.Printf("Prevhash: %s\n\n", block.prevHash)
+	}
+}
+
 func main() {
-	genesisBlock := block{"Genesis Block", "", ""}
-	hash := sha256.Sum256([]byte(genesisBlock.data + genesisBlock.prevHash))
-	hexHash := fmt.Sprintf("%x", hash)
-	genesisBlock.hash = hexHash
-	fmt.Println(hexHash)
+	chain := blockchain{}
+	chain.addBlock("Genesis Block")
+	chain.addBlock("Second Block")
+	chain.addBlock("Third Block")
+	chain.listBlocks()
 
 }
