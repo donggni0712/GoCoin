831cdb397e4d44796aebf6ecfa84441ba1a83b6c
donggni0712 <donggni0712@naver.com>
2022-06-15 19:21:16 +0900

    :ice_cube:Block Hash With Sha256

--- GoCoin/main.go
+++ GoCoin/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"crypto/sha256"
+	"fmt"
+)
+
+type block struct {
+	data     string
+	hash     string
+	prevHash string
+	/*
+		B1
+			B1Hash = (data +  'X')
+		B2
+			B2hash = (data + B1Hash)
+		B3
+			B3Hash = (data + B2Hash)
+		==> 이전 블록을 변경하면 이 블록이 무의미해짐 ==> 변경 불가능
+	*/
+}
 
 func main() {
-	fmt.Println("start")
+	genesisBlock := block{"Genesis Block", "", ""}
+	hash := sha256.Sum256([]byte(genesisBlock.data + genesisBlock.prevHash))
+	hexHash := fmt.Sprintf("%x", hash)
+	genesisBlock.hash = hexHash
+	fmt.Println(hexHash)
+
 }
