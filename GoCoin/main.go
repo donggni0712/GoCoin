b131d423ae18debe23747c7d84ccc466af29c021
donggni0712 <donggni0712@naver.com>
2022-06-16 19:40:20 +0900

    :globe_with_meridians:Start SSR Explorer

--- GoCoin/main.go
+++ GoCoin/main.go
@@ -2,17 +2,21 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/donggni0712/GoCoin/blockchain"
+	"log"
+	"net/http"
 )
 
+const port string = ":4000"
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello from home!")
+}
+
 func main() {
-	chain := blockchain.GetBlockchain()
-	chain.AddBlock("Seccond Block")
-	chain.AddBlock("Third Block")
-	for _, block := range chain.AllBlocks() {
-		fmt.Printf("Data: %s\n", block.GetData())
-		fmt.Printf("Hash: %s\n", block.GetHash())
-		fmt.Printf("PrevHash: %s\n\n", block.GetPrevHash())
-	}
+	http.HandleFunc("/", home)
+	fmt.Printf("Listrning on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
+	//http.ListenAndServe : 서버 실행, 에러가 있으면 에러 리턴
+	//log.Fatal : os.Exit(1)이후에 erro를 출력
+	//os.Exit(1) : error code 1으로 종료
 }
