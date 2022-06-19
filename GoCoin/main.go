6c273246e999542db0bc99eaf2a9f78d6a0e1e32
donggni0712 <donggni0712@naver.com>
2022-06-19 23:19:20 +0900

    :desktop_computer:Use NewEncoder And 'Field Struct Tag'

--- GoCoin/main.go
+++ GoCoin/main.go
@@ -5,17 +5,16 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-
-	"github.com/donggni0712/GoCoin/utils"
 )
 
 const port string = ":4000"
 
 type URLDescription struct {
-	URL         string
-	Method      string
-	Description string
-}
+	URL         string `json:"url"` //field struct tag
+	Method      string `json:"method"`
+	Description string `json:"description"`
+	Payload     string `json:"payload,omitempty"` // omitempty : 빈 값은 출력x #띄어쓰면 안됨
+} // `json:"-"` => field를 무시
 
 func documentation(w http.ResponseWriter, r *http.Request) {
 	data := []URLDescription{
@@ -24,16 +23,33 @@ func documentation(w http.ResponseWriter, r *http.Request) {
 			Method:      "GET",
 			Description: "See Documentation",
 		},
+		{
+			URL:         "/blocks",
+			Method:      "POST",
+			Description: "See Documentation",
+			Payload:     "data:string",
+		},
 	}
+
+	w.Header().Add("Content-Type", "application/json")
+	// firfox등에서 이쁘게 꾸며줌. json이라고 인지
+
 	// json.Marshal : struct => JSON
 	// b는 []byte
-	b, err := json.Marshal(data)
-	utils.HandleErr(err)
-	fmt.Printf("%s", b)
+
+	/*
+		b, err := json.Marshal(data)
+		utils.HandleErr(err)
+		fmt.Fprintf(w, "%s", b)
+		위 3줄 = json.NewEncoder(w).Encode(data)
+	*/
+	json.NewEncoder(w).Encode(data)
+
 }
 
 func main() {
 	//explorer.Start(4000)
-	fmt.Printf("Listening on http://localhost%s", port)
+	http.HandleFunc("/", documentation)
+	fmt.Printf("Listening on http://localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
