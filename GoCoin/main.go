8a952a77143b8f77953f4e65008ebca7f44d3c50
donggni0712 <donggni0712@naver.com>
2022-06-19 23:00:41 +0900

    :desktop_computer:Set For Rest API

--- GoCoin/main.go
+++ GoCoin/main.go
@@ -1,7 +1,39 @@
 package main
 
-import explorer "github.com/donggni0712/GoCoin/explorer/templates"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/donggni0712/GoCoin/utils"
+)
+
+const port string = ":4000"
+
+type URLDescription struct {
+	URL         string
+	Method      string
+	Description string
+}
+
+func documentation(w http.ResponseWriter, r *http.Request) {
+	data := []URLDescription{
+		{
+			URL:         "/",
+			Method:      "GET",
+			Description: "See Documentation",
+		},
+	}
+	// json.Marshal : struct => JSON
+	// b는 []byte
+	b, err := json.Marshal(data)
+	utils.HandleErr(err)
+	fmt.Printf("%s", b)
+}
 
 func main() {
-	explorer.Start(4000)
+	//explorer.Start(4000)
+	fmt.Printf("Listening on http://localhost%s", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
